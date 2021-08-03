268b978daedd324fc841321ec98cbdd62a040f84
ESTEBAN QUINTERO <78872778+jesteban39@users.noreply.github.com>
2021-08-03 14:12:30 -0500

    ejemplos en modelx

--- api/src/models/Modelx.ts
+++ api/src/models/Modelx.ts
@@ -1,8 +1,38 @@
-import { Model, Column, Table } from 'sequelize-typescript';
-import {HasMany, HasOne, BelongsTo, BelongsToMany, ForeignKey} from 'sequelize-typescript'
+import {
+  Model, Column, Table, DataType,
+  HasMany, HasOne, BelongsTo, BelongsToMany, ForeignKey
+} from 'sequelize-typescript'
+
 
 @Table
 export class Modelx extends Model<Modelx> {
+
+  /*  options: 
+string  ->	STRING
+boolean ->	BOOLEAN
+number  ->  INTEGER
+bigint  ->	BIGINT
+Date    ->	DATE
+Buffer  ->  BLOB
+ */
+
   @Column
   name!: string;
+
+  @Column(DataType.TEXT)
+  dato1!: string;
+
+  @Column({
+    type: DataType.FLOAT,
+    comment: 'Some value',
+  })
+  dato2!: number;
+
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    allowNull: false
+  })
+  dato3!: number;
+
 }
