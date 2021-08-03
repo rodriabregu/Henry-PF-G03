d6af5623d0cb26d5de770db32227b0d9faed7499
ESTEBAN QUINTERO <78872778+jesteban39@users.noreply.github.com>
2021-08-03 00:05:47 -0500

    not changed

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,15 +1,15 @@
-import {Router} from 'express';
-const router=Router();
+import { Router } from 'express';
+
+const router = Router();
+
 import productRoute from './products';
 import categoryRoute from './categories';
-
-import users from './users';
+import usersRoute from './users';
 import rutax from './rutax';
 
-router.use('/products',productRoute)
-router.use('/categories',categoryRoute)
-
-router.use('/users', users);
+router.use('/products', productRoute);
+router.use('/categories', categoryRoute);
+router.use('/users', usersRoute);
 router.use('/rutax', rutax);
 
 export default router;
