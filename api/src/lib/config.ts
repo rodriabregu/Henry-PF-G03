e2f6a28c08dd29e1769accf802d468ae4e4a1c47
ESTEBAN QUINTERO <78872778+jesteban39@users.noreply.github.com>
2021-08-03 00:38:44 -0500

    update .env

--- api/src/lib/config.ts
+++ api/src/lib/config.ts
@@ -6,7 +6,7 @@ const config = {
   dbUser: process.env.DB_USER || 'postgres',
   dbPassword: process.env.DB_PASSWORD || '',
   dbHost: process.env.DB_HOST || 'localhost',
-  dbName: process.env.DB_NAME || 'socialclub',
+  dbName: process.env.DB_NAME || '',
   dbPort: process.env.DB_PORT || '5432',
   dev: process.env.NODE_ENV !== 'production',
   port: process.env.API_PORT || '3001',
