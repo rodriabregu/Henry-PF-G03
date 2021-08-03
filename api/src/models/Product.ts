7b316b8c37dcc03f64472390f508e36e71d6323c
ESTEBAN QUINTERO <78872778+jesteban39@users.noreply.github.com>
2021-08-03 14:56:09 -0500

    add HasMany to Photo

--- api/src/models/Product.ts
+++ api/src/models/Product.ts
@@ -1,27 +1,32 @@
 import { Model, Column, Table } from 'sequelize-typescript';
-import {HasMany, HasOne, BelongsTo, BelongsToMany, ForeignKey} from 'sequelize-typescript'
+import { HasMany, HasOne, BelongsTo, BelongsToMany, ForeignKey } from 'sequelize-typescript'
+
 import { Photo } from './Photo';
 
 @Table
 export class Product extends Model<Product> {
-  @Column
+
+  @Column({ unique: true })
   name!: string;
-  
+
+  @HasMany(() => Photo)
+  photos!: Photo[]
+
   @Column
   description!: string;
-  
+
   @Column
   amount!: string;
 
   @Column
   stock!: string;
-  
+
   @Column
   brand!: string;
 
   @Column
   sportID!: string;
-  
+
   @Column
   category!: string;
 
