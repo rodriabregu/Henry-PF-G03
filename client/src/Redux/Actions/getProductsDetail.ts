05f3c9a19681758af2a010532656d0880c97ede8
Waldo Vázquez <76174982+waldovazquez@users.noreply.github.com>
2021-08-03 20:31:24 -0300

    fix bugs front

--- client/src/Redux/Actions/getProductsDetail.ts
+++ client/src/Redux/Actions/getProductsDetail.ts
@@ -1,13 +1,9 @@
-// import axios from "axios"
 import { Dispatch } from 'react';
-
 export const GET_PRODUCTS_DETAIL = 'GET_PRODUCTS_DETAIL';
 
-export function getProductDetail(id: number) {
-  return function (dispatch: Dispatch<any>) {
-    return dispatch({
-      type: GET_PRODUCTS_DETAIL,
-      payload: id,
-    });
-  };
+export function getProductsDetail(dispatch: Dispatch<any>, id: number) {
+  return dispatch({
+    type: GET_PRODUCTS_DETAIL,
+    payload: id,
+  });
 }
