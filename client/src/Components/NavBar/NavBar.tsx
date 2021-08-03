34e4a769b70442c8c9370dfb4fcf0e50d0601b01
Gabriela Lescano <gab.f.lescano@gmail.com>
2021-08-03 12:22:48 -0300

    Update NavBar.tsx

--- client/src/Components/NavBar/NavBar.tsx
+++ client/src/Components/NavBar/NavBar.tsx
@@ -1,8 +1,21 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
 export const NavBar = () => {
   return (
     <div>
-      
+    
+            <Link to='/home'> HOME </Link>
+
+            <Link to='/cart'> COMPRAS </Link>
+
+            <Link to='/config'> AJUSTES </Link>
+
+            <Link to='/register'> REGISTRARSE </Link>
+
+            <Link to='/login'> LOGIN </Link>
     </div>
+
   )
 };
 
