f7662241555f971ae487dcd07cd8d298f6989fe4
Mariano Galli <65736968+marianogalli@users.noreply.github.com>
2021-08-04 14:47:05 -0300

    Model y route category

--- api/src/models/Category.ts
+++ api/src/models/Category.ts
@@ -3,18 +3,12 @@ import {Optional} from 'sequelize'
 import {Product} from './Product'
 import { ProductCategory } from './ProductCategory';
 
-interface ICategory{
-    id:number,
-    description:string
-}
-
-interface CategoryCreationAttributes extends Optional<ICategory,'id'>{}
 
 @Table
-export class Category extends Model<ICategory,CategoryCreationAttributes>{
+export class Category extends Model{
 
     @Column
-    descritpion!:string;
+    description!:string;
 
     @BelongsToMany(()=>Product,()=>ProductCategory)
     products!:Product[]
