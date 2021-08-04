e917c0e25f01d8cabdc1bb6481ad03275d76d723
Gabriela Lescano <gab.f.lescano@gmail.com>
2021-08-04 12:22:04 -0300

    add Footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import './App.css';
 import Home from './Components/Home/Home';
 import NavBar from './Components/NavBar/NavBar';
+import Footer from './Components/Footer/Footer';
 import {BrowserRouter as Router,
   Switch,
   Route,} from 'react-router-dom'
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         <div className="App">
           <Route path="/" component={NavBar} />
           <Route exact path="/home" component={Home} />
+          <Route path="/" component={Footer} />
         </div>
       </Switch>
     </Router>
