fd5b7e086ca2fd2699f495e3a269b5886a0dad0f
ESTEBAN QUINTERO <78872778+jesteban39@users.noreply.github.com>
2021-08-04 02:41:08 -0500

    add getProductsCategory

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 const router = Router();
 
 import productRoute from './products';
+import getProductsCategory from './getProductsCategory';
 import categoryRoute from './categories';
 import usersRoute from './users';
 import rutax from './rutax';
 import categoryTypeRoute from './categoryTypes'
 
+router.use('/products/category/', getProductsCategory);
 router.use('/products', productRoute);
 router.use('/categories', categoryRoute);
 router.use('/users', usersRoute);
