27abbc66cf5a5fa66d908f460161caf494aaea6b
Gabriela Lescano <gab.f.lescano@gmail.com>
2021-08-04 15:14:10 -0300

    NavBar styles

--- client/src/Components/NavBar/NavBar.tsx
+++ client/src/Components/NavBar/NavBar.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './NavBar.css';
+import {RiShoppingCartLine, RiSettings5Line, RiAccountCircleLine, RiHome2Line} from 'react-icons/ri';
+
 
 export const NavBar = () => {
   return (
-    <div>
-      <Link to='/home'> HOME </Link>
-
-      <Link to='/cart'> COMPRAS </Link>
-
-      <Link to='/config'> AJUSTES </Link>
-
-      <Link to='/register'> REGISTRARSE </Link>
-
-      <Link to='/login'> LOGIN </Link>
-    </div>
+    <header>
+      <nav>
+        <div className='nav-container'>
+          <div className='link-home'>
+          <Link to='/home'> HOME <RiHome2Line/> </Link>
+          </div>
+          <div className='links-compra'>
+            <Link to='/cart'> CART <RiShoppingCartLine/></Link>
+            
+            <Link to='/config'> SETTINGS <RiSettings5Line/> </Link>
+          </div>
+          <div className='links-login'>
+            <Link to='/register'> REGISTER </Link>
+          
+            <Link to='/login'> LOGIN <RiAccountCircleLine/> </Link>
+          </div>
+        </div>
+        </nav>
+    </header>
   );
 };
 
