6c63824555f422346274904303a5c9d4e9ac7342
Esteban Planells <planells_e@yahoo.com.ar>
2021-08-04 22:56:32 -0300

    Esteban Product details by id
    ruta que devuelve el detalle del producto accediendo por ID

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -7,13 +7,13 @@ import categoryRoute from './categories';
 import usersRoute from './users';
 import rutax from './rutax';
 import categoryTypeRoute from './categoryTypes';
-import productDetails from './productDetails';
+import getProductById from './getProductById';
 
 router.use('/products', productsRoute);
 router.use('/categories', categoryRoute);
 router.use('/users', usersRoute);
 router.use('/rutax', rutax);
 router.use('/categoryTypes',categoryTypeRoute)
-router.use('/productDetails',productDetails)
+router.use('/getproductById',getProductById)
 
 export default router;
