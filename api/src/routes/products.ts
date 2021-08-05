c539e5cc2ab918fa500455a43e45c5d857af78b2
ESTEBAN QUINTERO <78872778+jesteban39@users.noreply.github.com>
2021-08-05 12:15:16 -0500

    responde con los datos en db

--- api/src/routes/products.ts
+++ api/src/routes/products.ts
@@ -3,9 +3,21 @@ import { Router, Request, Response } from 'express';
 import { addProduct } from '../providers';
 import { appProduct } from "../@app"
 
+import { sequelize } from '../db';
+const { Product, Photo } = sequelize.models;
+
 const router = Router();
+
 router.get('/', (req: Request, res: Response) => {
-  res.send('estoy en get productos')
+  return Product.findAll({
+    include: "photos",
+  }).then((products) => {
+    return res.json({
+      message: 'Success',
+      data: products
+    })
+  })
+
 })
 
 router.post('/', (req: Request, res: Response) => {
@@ -33,8 +45,6 @@ router.post('/', (req: Request, res: Response) => {
 })
 
 
-import { sequelize } from '../db';
-const { Product, Photo } = sequelize.models;
 
 router.delete('/', (_req: Request, res: Response) => {
 
