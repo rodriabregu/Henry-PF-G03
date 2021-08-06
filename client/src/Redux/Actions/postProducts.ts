9967fc2be768e87b616f735f8f24cc6526a4dd44
Rodrigo Alcino <rodrigo_alcino@hotmail.com>
2021-08-06 14:45:05 -0300

    fix searchbar

--- client/src/Redux/Actions/postProducts.ts
+++ client/src/Redux/Actions/postProducts.ts
@@ -2,7 +2,6 @@ import { Dispatch } from 'redux';
 import axios from 'axios';
 export const POST_PRODUCTS = 'POST_PRODUCTS';
 
-
 export function postProducts(payload:any) {
   return function(dispatch:any):any{
         axios.post('http://localhost:3001/products', payload)
