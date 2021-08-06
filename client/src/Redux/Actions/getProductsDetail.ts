12983f401b1371203fcdc0e10ff03c31afb20f37
Mariano Galli <65736968+marianogalli@users.noreply.github.com>
2021-08-06 20:01:08 -0300

    Product detail funciona ok

--- client/src/Redux/Actions/getProductsDetail.ts
+++ client/src/Redux/Actions/getProductsDetail.ts
@@ -6,7 +6,6 @@ export const GET_PRODUCTS_DETAIL = 'GET_PRODUCTS_DETAIL';
 export function getProductsDetail(id:number) {
   return async (dispatch:any) => {
     const res:any = await axios.get(`http://localhost:3001/product/id/${id}`)
-    console.log(res.data)
     dispatch({ type: GET_PRODUCTS_DETAIL, payload: res.data })
   }
 }
