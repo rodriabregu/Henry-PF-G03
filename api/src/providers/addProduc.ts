0de43041cb2fe510014b40d238d4d9c6deaede5a
ESTEBAN QUINTERO <78872778+jesteban39@users.noreply.github.com>
2021-08-06 12:46:45 -0500

    update get post products

--- api/src/providers/addProduc.ts
+++ api/src/providers/addProduc.ts
@@ -4,16 +4,15 @@ import { appProduct } from '../@app'
 
 export default async function (
   product: appProduct,
-  photos?: string[]
+  photos: string[]
 ): Promise<number> {
 
   const neWProduct = await Product.findOrCreate({ where: { ...product } })
   const productId = await neWProduct[0].getDataValue('id')
 
-  Photo.findOrCreate({ where: { url: product.photo, productId } })
-  if (photos) await Promise.all(photos ? photos.map(photo => {
+  await Promise.all(photos.map(photo => {
     return Photo.findOrCreate({ where: { url: photo, productId } })
-  }) : [])
+  }))
 
   return neWProduct[0].getDataValue('id');
 
