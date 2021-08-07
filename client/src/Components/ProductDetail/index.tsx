a57adef7bfc3ebf221a0459c899cade79f12abad
Mariano Galli <65736968+marianogalli@users.noreply.github.com>
2021-08-06 23:17:50 -0300

    Detalle producto muestra todas las imagenes

--- client/src/Components/ProductDetail/index.tsx
+++ client/src/Components/ProductDetail/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getProducts } from '../../Redux/Actions/getProducts';
@@ -18,11 +18,27 @@ const ProductDetail = () => {
 
     useEffect(() => {
         dispatch(getProductsDetail(parseInt(id)));
-        console.log('detail ',detail)
+        console.log('detail ', detail)
     }, [dispatch]);
 
+    const [photo, setPhoto] = useState(0);
+
+    const changePhoto=(e:any)=>{
+        const action=e.target.name;
+        if(action==='next'){
+            if(photo<detail.photos.length-1){
+                setPhoto(photo+1)
+            }
+        }else{
+            if(photo>0){
+                setPhoto(photo-1)
+            }
+        }
+        
+    }
+
     return (
-        <div> 
+        <div>
             <div className='button'>
                 <Link to='/home'>
                     <button onClick={dispatch<any>(getProducts())}>
@@ -30,16 +46,25 @@ const ProductDetail = () => {
                     </button>
                 </Link>
             </div>
-            
-             <div className='detailgeneral'>
+
+            <div className='detailgeneral'>
+                <button name='prev' onClick={changePhoto}>Anterior</button>
+                <button name='next' onClick={changePhoto}>Siguiente</button>
                 <div className='product-detail'>
                     <h1 className='title'>{detail.name}</h1>
                     <h2>${detail.price}.00</h2>
                     <h3>Size: {detail.size}</h3>
                     <h3>Review: {detail.review}</h3>
                 </div>
-                <div className='product-img'><img src={detail.photos?detail.photos[0].url:''} alt='img not found' /></div>
-            </div> 
+                <div>
+                    {
+                        detail.photos?detail.photos.map((f:any)=><img src={f.url} width='50px'></img>):''
+                    }
+                </div>
+                <div className='product-img'>
+                    <img src={detail.photos ? detail.photos[photo].url : ''} alt='img not found' />
+                </div>
+            </div>
 
         </div>
     )
