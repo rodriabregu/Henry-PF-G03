a52ce23d12bddad7ac2141339272ef611c24b8bd
Rodrigo Alcino <rodrigo_alcino@hotmail.com>
2021-08-11 20:57:21 -0300

    Cart finish

--- client/src/Components/ProductDetail/index.tsx
+++ client/src/Components/ProductDetail/index.tsx
@@ -137,8 +137,10 @@ const ProductDetail = () => {
                                     )
                                 })
                             }
-                            <AddCart 
+                            <AddCart
+                            id={detail.id}
                             name={detail.name} 
+                            photo={detail.photos}
                             stock={detail.stock} 
                             price={detail.price} 
                             description={detail.description} 
--- client/src/Components/Products/addCart.js
+++ client/src/Components/Products/addCart.js
@@ -1,16 +1,21 @@
+import { useState} from 'react';
 import { useDispatch } from 'react-redux';
 import { TiShoppingCart } from 'react-icons/ti';
 import { addActionCart } from '../../Redux/Actions/Products/addingCart'
 
-const AddCart = ({name, stock, price, brand, description, categories}) => {
+const AddCart = ({id, name, photo, stock, price, brand, description, categories}) => {
     const dispatch = useDispatch();
+    const [value, setValue] = useState({ value: 1 })
     const product = {
+        id,
         name,
+        photo,
         stock, 
         price,
         brand,
         description,
         categories,
+        value,
     };
 
     const addProduct = () => {
@@ -20,19 +25,30 @@ const AddCart = ({name, stock, price, brand, description, categories}) => {
         }
         products.push(product);
         localStorage.setItem('products-cart', JSON.stringify(products));
-    }
+    };
 
     const addCart = product => {
         dispatch(addActionCart(product));
         addProduct();
     };
 
+    const handleChange = (e) => {
+        setValue({value: e.target.value});
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <div>
+            <form onSubmit={handleSubmit} >
             <button
                 onClick={() => addCart()}>
                 Add to cart <TiShoppingCart />
             </button>
+                <input type='number' min="1" max={stock} defaultValue='1' onChange={handleChange} />
+            </form>
         </div>
     )
 };
