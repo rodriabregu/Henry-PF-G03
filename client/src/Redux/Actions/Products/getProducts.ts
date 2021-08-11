666fa2cc876ecfd629adf8ae5b9264294c754b18
Mariano Galli <65736968+marianogalli@users.noreply.github.com>
2021-08-11 18:54:00 -0300

    Filtros funcionando

--- client/src/Redux/Actions/Products/getProducts.ts
+++ client/src/Redux/Actions/Products/getProducts.ts
@@ -4,6 +4,7 @@ export const GET_PRODUCTS = 'GET_PRODUCTS';
 export function getProducts() {
   return async (dispatch:any) => {
     const res:any = await axios.get('http://localhost:3001/products')
+    console.log('productos ',res.data.data)
     dispatch({ type: GET_PRODUCTS, payload: res.data.data })
   };
 };
