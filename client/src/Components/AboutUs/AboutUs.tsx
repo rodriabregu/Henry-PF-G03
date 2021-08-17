d083abd3418b66f975b806f4f427663c3b2856bf
Gabriela Lescano <gab.f.lescano@gmail.com>
2021-08-17 00:32:52 -0300

    About, Contact, FAQ

---
+++ client/src/Components/AboutUs/AboutUs.tsx
@@ -0,0 +1,12 @@
+import Loading from "../Loading/Loading";
+
+export const AboutUs = () => {
+    return(
+        <div>
+            <Loading/>
+      
+        </div>
+    )
+}
+
+export default AboutUs;
---
+++ client/src/Components/ContactUs/ContactUs.tsx
@@ -0,0 +1,12 @@
+import Loading from "../Loading/Loading";
+
+export const ContactUs = () => {
+    return(
+        <div>
+            <Loading/>
+      
+        </div>
+    )
+}
+
+export default ContactUs;
