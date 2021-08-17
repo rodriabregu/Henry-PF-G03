6dde6eaec66f1f14c984819090ea7f4997b9417e
ESTEBAN QUINTERO <78872778+jesteban39@users.noreply.github.com>
2021-08-16 19:53:42 -0500

    sin all

--- client/src/Components/Products/SelectCategory.tsx
+++ client/src/Components/Products/SelectCategory.tsx
@@ -5,7 +5,7 @@ import config from '../../../src/config';
 
 const SelectCategory = (props:any) => {
     const { path } = props;
-    const [elements, setelements] = useState([{id:0, name:"all"}]);
+    const [elements, setelements] = useState([{id:0, name:""}]);
 
     useEffect(()=>{
         axios.get(`http://${config.REACT_APP_API_URL}:3001/api/${path}`)
@@ -15,7 +15,7 @@ const SelectCategory = (props:any) => {
     }, [path])
     return(
         <select className={props.className} name={props.name} onChange={props.onChange}>
-            {elements ?[{id:0, name:"all"}, ...elements].map( (c:any) => <option id={c.id}>{c.name}</option>) : '' }
+            {elements ?[{id:0, name:""}, ...elements].map( (c:any) => <option id={c.id}>{c.name}</option>) : '' }
         </select>
     )
 };
