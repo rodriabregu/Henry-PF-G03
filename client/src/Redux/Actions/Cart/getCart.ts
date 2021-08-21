f4b0a5ff72d6a2b67839ca6917cfa6dc07a84497
ESTEBAN QUINTERO <78872778+jesteban39@users.noreply.github.com>
2021-08-20 21:00:21 -0500

    update user?.sub

--- client/src/Redux/Actions/Cart/getCart.ts
+++ client/src/Redux/Actions/Cart/getCart.ts
@@ -2,26 +2,25 @@ import axios from 'axios';
 import config from '../../../../src/config'
 import { UPDATE_CART, item } from './updateCart'
 
-export const getCart = () => {
-  let isLogin: boolean = true
-  const userId: number = 1
+export const getCart = ( userId: string | undefined) => {
+
   return async (dispatch: Function) => {
     try {
-      if (isLogin) {
+      const items: item[] = JSON.parse(
+        localStorage.getItem('products-cart') || "[]"
+      )
+
+      if (userId) {
         const res = await axios.get(
           `http://${config.REACT_APP_API_URL}:${config.port}/api/cart/${userId}`
         )
-        return dispatch({
-          type: UPDATE_CART,
-          payload: res.data.data.items
-        })
-      } else {
-        const items: item[] = JSON.parse(localStorage.getItem('products-cart') || "[]")
-        return dispatch({
-          type: UPDATE_CART,
-          payload: items
-        })
+        items.concat(res.data.data.items)
       }
+
+      return dispatch({
+        type: UPDATE_CART,
+        payload: items
+      })
 
     } catch (error) {
       console.error(error.message || error)
