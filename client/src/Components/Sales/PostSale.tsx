7abe78940592c5c66f8b1539b3cc5612da65dff3
ESTEBAN QUINTERO <78872778+jesteban39@users.noreply.github.com>
2021-08-23 16:01:11 -0500

    update postSale

--- client/src/Components/Sales/PostSale.tsx
+++ client/src/Components/Sales/PostSale.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useLocation, useParams } from 'react-router'
 import './PostSale';
 import config from '../../config'
@@ -7,34 +7,32 @@ import PaySuccessful from './PaySuccesful';
 import ErrorPay from './ErrorPay';
 
 export default function PostSale() {
-    const { saleId } = useParams<any>();
+  const { saleId } = useParams<any>();
+  const [sales, setSales] = useState<any>('')
+  const { search } = useLocation()
 
-    const [sales, setSales] = useState<any>('')
-
-    const { search } = useLocation()
-
+  useEffect(() => {
     let saleState: string = ""
+    axios.put(`http://${config.REACT_APP_API_URL}:${config.port}/api/sale` + search, { saleId })
+      .then(res => {
+        saleState = res?.data?.data?.state
+        setSales(saleState)
+        console.log('saleState', res)
+      });
+  })
 
-    axios.put(`http://${config.REACT_APP_API_URL}:${config.port}/api/sale` + search, {saleId})
-            .then(res => {
-                saleState = res?.data?.data?.state
-                setSales(saleState)
-                localStorage.removeItem('products-cart')
-                console.log('saleState', res)
-            });
+  return (
+    <div>{
+      sales === "Created" ?
+        <div>
+          {/* En caso de que el producto haya sido pagado satisfactoriamente */}
+          <PaySuccessful />
+        </div> :
 
-    return (
-        <div>{
-            sales === "Created" ? 
-            <div>
-                {/* En caso de que el producto haya sido pagado satisfactoriamente */}
-                <PaySuccessful />
-            </div> : 
-            
-            <div>
-                {/* En caso de estar pendiente o cancelado */}
-                <ErrorPay />
-            </div>
-        }</div>
-    )
+        <div>
+          {/* En caso de estar pendiente o cancelado */}
+          <ErrorPay />
+        </div>
+    }</div>
+  )
 };
