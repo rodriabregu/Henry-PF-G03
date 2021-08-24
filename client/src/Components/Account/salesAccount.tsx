f8f7a8337801f806576d392464f98f021a70a112
Leonardo Catto <leocatto96@gmail.com>
2021-08-23 23:19:14 -0300

    Leo&Rodri

--- client/src/Components/Account/salesAccount.tsx
+++ client/src/Components/Account/salesAccount.tsx
@@ -1,7 +1,10 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getSales } from '../../Redux/Actions/Sales/getSale';
+import { NavLink } from 'react-router-dom';
 import { useAuth0 } from "@auth0/auth0-react";
+import './salesAccount.css'
+import moment from 'moment';
 
 const SalesAccount = () => {
     const dispatch = useDispatch()
@@ -12,20 +15,32 @@ const SalesAccount = () => {
     
     const filterUser = allSales?.filter((f:any) => f.userId === user?.sub);
 
+    console.log('give me the filter', filterUser)
+
     useEffect( () => {
         dispatch(getSales())
-    }, []);
+    },[]);
 
     return (
-        <div>
+        <div className='salesAccountcss'>
             {
-                filterUser.map((f:any) => {
+                filterUser?.map((f:any) => {
                     return ( 
                         <>
-                        <div>
-                            <span>{f}</span>             
-                            <span>{f.items}</span>             
-                            <span>{f.state}</span>
+                        <div className='s-details'> 
+                            {<span>Date: {moment(f.date).format("DD-MM-YYYY HH:mm")}</span>}           
+                            {f.items?.map((x:any)=>{
+                                return(
+                                    <div>
+                                        <NavLink to={`/product/${x.productId}`}>
+                                            <span>{x.productName}{<br/>}</span>
+                                        </NavLink>
+                                        <span>Price: {x.salePrice}{<br/>}</span>
+                                        <span>Units: {x.units}</span>
+                                    </div>
+                                )
+                            })}          
+                            <span>State: {f.state}</span>
                         </div>             
                         </>
                     )
