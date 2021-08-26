034701d8068094747e12e3107a4468239f82ff84
ESTEBAN QUINTERO <78872778+jesteban39@users.noreply.github.com>
2021-08-25 23:17:56 -0500

    add axios.get en logOut

--- client/src/Components/LogOut/index.tsx
+++ client/src/Components/LogOut/index.tsx
@@ -1,19 +1,29 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from 'react';
+import config from '../../../src/config';
+import axios from "axios";
 import './logout.css';
 
 const Logout = () => {
-const { logout } = useAuth0();
-const { user } = useAuth0();
+  const { logout } = useAuth0();
+  const { user } = useAuth0();
 
-logout();
+  logout();
+  useEffect(() => {
+    return () => {
+      axios.get(
+        `http://${config.REACT_APP_API_URL}:${config.port}/api/cart/${user?.sub}`
+      )
+    }
+  }, []);
 
-    localStorage.clear();
+  localStorage.clear();
 
-    return (
-        <div className='form-register'>
-            <h1>Come back soon {user?.name}!</h1>
-        </div> 
-    );
+  return (
+    <div className='form-register'>
+      <h1>Come back soon {user?.name}!</h1>
+    </div>
+  );
 };
 
 export default Logout;
