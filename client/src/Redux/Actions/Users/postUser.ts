6d2a3716231c2fb0ed6f192abe6e1117ab6410b8
ESTEBAN QUINTERO <78872778+jesteban39@users.noreply.github.com>
2021-08-26 17:32:03 -0500

    update

--- client/src/Redux/Actions/Users/postUser.ts
+++ client/src/Redux/Actions/Users/postUser.ts
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import config from '../../../../src/config';
 import { user } from '../../../typesApp'
-export const UDATE_USER = 'UDATE_USER';
+export const UPDATE_USER = 'UPDATE_USER';
 
 export const postUser = (dataUser: user) => {
     return async (dispatch: Function) => {
         await axios.post(`http://${config.REACT_APP_API_URL}:${config.port}/api/user`, dataUser)
             .then(res => {
                 dispatch({
-                    type: UDATE_USER,
+                    type: UPDATE_USER,
                     payload: res.data.user
                 })
             })
