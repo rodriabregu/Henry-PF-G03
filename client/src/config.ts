a93acdab4615e9a68756a3b67b27a13a0d7ac982
ESTEBAN QUINTERO <78872778+jesteban39@users.noreply.github.com>
2021-08-26 22:04:04 -0500

    grupo03.sytes.net

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-  REACT_APP_API_URL: process.env.API_HOST || 'localhost',
-  REACT_APP_CLIENT_URL: process.env.CLIENT_URL || 'localhost',
+  REACT_APP_API_URL: process.env.API_HOST || 'grupo03.sytes.net',
+  REACT_APP_CLIENT_URL: process.env.CLIENT_URL || 'grupo03.sytes.net',
   REACT_APP_CLIENT_PORT: process.env.CLIENT_PORT || '3000', //80
   port: process.env.API_PORT || '3001'
 };
